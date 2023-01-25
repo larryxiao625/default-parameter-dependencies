ae6e07245ca55cbff62ef1fcf37522a1cd6601d0
xiaozihan.larryxiao <xiaozihan.larryxiao@bytedance.com>
2023-01-25 23:29:07 +0800

    [opt] Add defaultParameter0

--- dependency/src/main/java/org/example/DefaultParameter.kt
+++ dependency/src/main/java/org/example/DefaultParameter.kt
@@ -3,6 +3,7 @@ package org.example
 open class DefaultParameter {
     fun defaultParameterTest(
         normal: String,
+        defaultParameter0: String? = null,
         defaultParameter1: String = "",
         defaultParameter2: Any? = null
     ) {
