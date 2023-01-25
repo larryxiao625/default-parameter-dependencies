7c35f787e9d87504b571091c385de86bc6355eb5
xiaozihan.larryxiao <xiaozihan.larryxiao@bytedance.com>
2023-01-25 23:34:50 +0800

    [opt] Remove defaultParameter0

--- dependency/src/main/java/org/example/DefaultParameter.kt
+++ dependency/src/main/java/org/example/DefaultParameter.kt
@@ -3,7 +3,6 @@ package org.example
 open class DefaultParameter {
     fun defaultParameterTest(
         normal: String,
-        defaultParameter0: String? = null,
         defaultParameter1: String = "",
         defaultParameter2: Any? = null
     ) {
