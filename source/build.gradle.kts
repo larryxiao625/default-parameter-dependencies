f515725cd66f89322e47bdb996b6b9f2be3e1e95
xiaozihan.larryxiao <xiaozihan.larryxiao@bytedance.com>
2023-01-28 10:58:04 +0800

    [opt] Includ libs in local

--- source/build.gradle.kts
+++ source/build.gradle.kts
@@ -3,20 +3,9 @@ plugins {
     id("java")
 }
 
-repositories {
-    mavenCentral()
-    maven {
-        url = uri("https://maven.pkg.github.com/larryxiao625/default-parameter-dependencies")
-        credentials {
-            username = "larryxiao625"
-            password = "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"
-        }
-    }
-}
-
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.example.larryxiao625:default-parameter-dependency:v0.0.2")
+    implementation(files("libs/default-parameter-dependency-v0.0.7.jar"))
 }
 
 tasks.test {
--- source/src/main/kotlin/org/example/DefaultParameterInvoke.kt
+++ source/src/main/kotlin/org/example/DefaultParameterInvoke.kt
@@ -1,10 +1,8 @@
 package org.example
 
-import org.example.DefaultParameter
-
 class DefaultParameterInvoke {
 
     fun invokeDefaultParameter() {
-        DefaultParameter().defaultParameterTest("aaaa", defaultParameter1 = "aaaaa")
+        DefaultParameterImpl().defaultParameterTest("aaaa", defaultParameter1 = "aaaaa")
     }
 }
