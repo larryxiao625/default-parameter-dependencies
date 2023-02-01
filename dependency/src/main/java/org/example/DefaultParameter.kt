6622b84df78cfa783449ef72b5fc7902c770ccd0
xiaozihan.larryxiao <xiaozihan.larryxiao@bytedance.com>
2023-02-01 19:54:17 +0800

    [opt] Publish v0.0.14

--- dependency/src/main/java/org/example/DefaultParameter.kt
+++ dependency/src/main/java/org/example/DefaultParameter.kt
@@ -5,6 +5,7 @@ abstract class DefaultParameter {
         normal: T,
         defaultParameter1: String = "",
         defaultParameter2: ((Throwable) -> Unit)? = null,
+        defaultParameter21: ((T) -> Unit)? = null,
         defaultParameter3: (() -> Unit)? = null,
         defaultParameter4: ((T) -> Unit)? = null
     ) {
